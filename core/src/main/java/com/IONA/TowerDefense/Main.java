4d7a6e4bce4938529c6e56a5329daa1ef20876d3
MrMulleMeck <noel.elm04@hotmail.se>
2025-11-04 19:18:59 +0100

    Flyttade model till rätt ställe. MVC och Main hittas via: core/src/main/java/com.IONA.TowerDefense

--- core/src/main/java/com/IONA/TowerDefense/Main.java
+++ core/src/main/java/com/IONA/TowerDefense/Main.java
@@ -4,23 +4,26 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        font = new BitmapFont();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        font.draw(batch,"Tower Defense", 140, 210);
         batch.end();
     }
 
--- core/src/main/java/com/IONA/TowerDefense/model/TowerFire.java
+++ core/src/main/java/com/IONA/TowerDefense/model/TowerFire.java
@@ -1,4 +1,4 @@
-package model;
+package com.IONA.TowerDefense.model;
 
 public interface TowerFire {
     // TODO: No idea how this works when shooting a projectile isn't merely a state but an action, or what to return.
