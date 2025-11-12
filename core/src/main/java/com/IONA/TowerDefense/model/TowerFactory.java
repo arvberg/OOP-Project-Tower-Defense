b1dabd6b88a304ed360211a0ec1b80bd820d43a2
Ingmarolsen <olseningmar@gmail.com>
2025-11-12 13:09:12 +0100

    lade till Factory

---
+++ core/src/main/java/com/IONA/TowerDefense/model/TowerFactory.java
@@ -0,0 +1,18 @@
+package com.IONA.TowerDefense.model;
+
+import java.awt.*;
+
+public class TowerFactory {
+
+    public static Tower createTower (String type, Point pos, Dimension size) {
+
+        if (type.equalsIgnoreCase("TowerBasic")) {
+            return new TowerBasic(pos, size);
+        }
+        else if (type.equalsIgnoreCase("TowerFast")) {
+            return new TowerFast(pos, size);
+        }
+        else if (type.equalsIgnoreCase())
+
+    }
+}
