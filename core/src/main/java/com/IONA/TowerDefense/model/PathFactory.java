e324b9847452bc3a6953644b64eb72b0d728d4b9
Noel <noelel@chalmers.se>
2025-11-20 13:50:25 +0100

    Bettet path

--- core/src/main/java/com/IONA/TowerDefense/model/PathFactory.java
+++ core/src/main/java/com/IONA/TowerDefense/model/PathFactory.java
@@ -19,20 +19,20 @@ public class PathFactory {
     public static Path examplePath2() {
         return new Path(List.of(
             new Segment(new Vector2(0, 5), 3, Direction.EAST), // (2,6) -> (2,4) ner
-            new Segment(new Vector2(3, 5), 2, Direction.NORTH),  // (2,4) -> (5,4) höger
-            new Segment(new Vector2(3, 7), 2, Direction.EAST),
-            new Segment(new Vector2(5, 7), 4, Direction.SOUTH), // (2,6) -> (2,4) ner
-            new Segment(new Vector2(5, 3), 3, Direction.WEST),  // (2,4) -> (5,4) höger
-            new Segment(new Vector2(2, 3), 1, Direction.SOUTH),
+            new Segment(new Vector2(3, 5), 3, Direction.NORTH),  // (2,4) -> (5,4) höger
+            new Segment(new Vector2(3, 8), 2, Direction.EAST),
+            new Segment(new Vector2(5, 8), 4, Direction.SOUTH), // (2,6) -> (2,4) ner
+            new Segment(new Vector2(5, 4), 3, Direction.WEST),  // (2,4) -> (5,4) höger
+            new Segment(new Vector2(2, 4), 2, Direction.SOUTH),
             new Segment(new Vector2(2, 2), 9, Direction.EAST),
-            new Segment(new Vector2(11, 2), 3, Direction.NORTH),
-            new Segment(new Vector2(11, 5), 2, Direction.WEST),
-            new Segment(new Vector2(9, 5), 1, Direction.NORTH),
-            new Segment(new Vector2(9, 6), 2, Direction.EAST),
-            new Segment(new Vector2(11, 6), 1, Direction.NORTH),
-            new Segment(new Vector2(11, 7), 3, Direction.WEST),
-            new Segment(new Vector2(8, 7), 2, Direction.NORTH),
-            new Segment(new Vector2(8, 9), 0, Direction.NORTH)
+            new Segment(new Vector2(11, 2), 2, Direction.NORTH),
+            new Segment(new Vector2(11, 4), 4, Direction.WEST),
+            new Segment(new Vector2(7, 4), 2, Direction.NORTH),
+            new Segment(new Vector2(7, 6), 4, Direction.EAST),
+            new Segment(new Vector2(11, 6), 2, Direction.NORTH),
+            new Segment(new Vector2(11, 8), 4, Direction.WEST),
+            new Segment(new Vector2(7, 8), 2, Direction.NORTH),
+            new Segment(new Vector2(7, 10), 0, Direction.NORTH)
 
 
         ));
