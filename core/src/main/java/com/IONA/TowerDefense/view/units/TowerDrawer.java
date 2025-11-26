fd774255efa1b9a5a008b8b505ed77e00abc0238
Ingmarolsen <olseningmar@gmail.com>
2025-11-26 16:15:15 +0100

    ändrade radius

--- core/src/main/java/com/IONA/TowerDefense/view/units/TowerDrawer.java
+++ core/src/main/java/com/IONA/TowerDefense/view/units/TowerDrawer.java
@@ -2,6 +2,7 @@ package com.IONA.TowerDefense.view.units;
 
 import com.IONA.TowerDefense.model.units.towers.Tower;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.List;
@@ -17,8 +18,9 @@ public class TowerDrawer {
 
     public static void drawTower(Tower tower, SpriteBatch batch) {
         Vector2 p = tower.getPosition();
-
         batch.draw(tower.texture, p.x - 0.68f /2f, p.y - 0.9f / 2f , 0.68f, 0.9f);
     }
+
+
 
 }
