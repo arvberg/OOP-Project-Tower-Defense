500c880a4a99262b603d9303abada10b8db92b43
Olov <olovsvedjeland@hotmail.com>
2025-11-26 17:27:46 +0100

    initial TargetNearestEnemy

--- core/src/main/java/com/IONA/TowerDefense/model/units/towers/targetingStrategies/TargetNearestEnemy.java
+++ core/src/main/java/com/IONA/TowerDefense/model/units/towers/targetingStrategies/TargetNearestEnemy.java
@@ -1,4 +1,22 @@
 package com.IONA.TowerDefense.model.units.towers.targetingStrategies;
 
+import com.IONA.TowerDefense.model.units.enemies.Enemy;
+import com.IONA.TowerDefense.model.units.towers.Tower;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TargetNearestEnemy {
+    /*
+    @Override
+    public List<Enemy> pick(List<Enemy> enemies, Tower tower) {
+        List<Enemy> result = new ArrayList<>();
+        Enem
+        if (enemies.isEmpty()) {
+            return result;
+        }
+        for (Enemy enemy :  enemies) {
+        }
+    }
+     */
 }
