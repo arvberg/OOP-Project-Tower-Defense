35ce7278267d2adf176058af6cd14606023fcb37
Ingmarolsen <olseningmar@gmail.com>
2025-11-26 12:58:34 +0100

    fixade select, men fel offset

--- core/src/main/java/com/IONA/TowerDefense/controller/InputHandler.java
+++ core/src/main/java/com/IONA/TowerDefense/controller/InputHandler.java
@@ -5,6 +5,7 @@ import com.IONA.TowerDefense.model.ui.Button;
 import com.IONA.TowerDefense.model.ui.PlayButton;
 import com.IONA.TowerDefense.model.ui.TowerMenuItem;
 import com.IONA.TowerDefense.model.ui.TowerMenuToggleButton;
+import com.IONA.TowerDefense.model.units.towers.Tower;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class InputHandler {
     private final PlayButton playButton;
     private final TowerMenuToggleButton towermenutogglebutton;
     private final List<TowerMenuItem> towerMenuItems;
+    private final List<Tower> towers;
 
     private GameModel model;
 
@@ -23,20 +25,32 @@ public class InputHandler {
         this.towermenutogglebutton = model.getTowerMenuToggleButton();
         this.towerMenuItems = model.getTowerMenuItems();
         this.model = model;
+        this.towers = model.getTowers();
     }
 
 
     public void checkInput(Vector2 pos){
+        // Först UI-kontroller
         towermenutogglebutton.isClicked(pos);
         playButton.isClicked(pos);
         for (TowerMenuItem t : towerMenuItems) {
             t.isClicked(pos);
         }
-        //tower.isClicked(pos);
+
         // Om spelaren har ett torn redo att placera
         if (model.isBuyingState()) {
-            if (clickedOnGameArea(pos)) {   // Se nedan
+            if (clickedOnGameArea(pos)) {
                 model.placeTower(pos);
+            }// Tornet placeras
+            return; // Avsluta, vi ska inte välja torn ännu
+        }
+
+        // Endast välj torn om man INTE håller på att placera ett nytt
+        if (clickedOnGameArea(pos)) {
+            model.selectTower(pos);
+            if (model.isTowerSelected()) {
+                Tower selected = model.getSelectedTower();
+                System.out.println("Tower selected at: " + selected.getPosition());
             }
         }
     }
