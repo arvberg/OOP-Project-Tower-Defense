66480bc8451d2faec0e8360564219ff4018ad8a8
Ingmarolsen <olseningmar@gmail.com>
2025-11-26 13:40:47 +0100

    fixade offset för select/deselect

--- core/src/test/java/JUnitTest.java
+++ core/src/test/java/JUnitTest.java
@@ -1,7 +1,10 @@
 import com.IONA.TowerDefense.model.models.GameModel;
 import com.IONA.TowerDefense.model.units.enemies.Enemy;
 import com.IONA.TowerDefense.model.units.enemies.EnemyBasic;
+import com.IONA.TowerDefense.model.units.towers.Tower;
+import com.IONA.TowerDefense.model.units.towers.TowerBasic;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.math.Vector2;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,5 +20,21 @@ public class JUnitTest {
         model.addEnemy(enemy);
         model.removeEnemy(enemy);
         assertEquals(0, model.getEnemies().size());
+    }
+
+    @Test
+    public void texturePosTest() {
+        GameModel model = new GameModel();
+        Tower tower = new TowerBasic();
+
+        Vector2 placePos = new Vector2(5.0f, 5.0f);
+        tower.setPosition(placePos);
+        model.addTower(tower);
+
+        float expectedDrawX = placePos.x - tower.texture.getWidth() / 2f;
+        float expectedDrawY = placePos.y - tower.texture.getHeight() / 2f;
+
+        assertEquals(expectedDrawX, tower.getPosition().x - tower.texture.getWidth()/2f, 0.001f);
+        assertEquals(expectedDrawY, tower.getPosition().y - tower.texture.getHeight()/2f, 0.001f);
     }
 }
