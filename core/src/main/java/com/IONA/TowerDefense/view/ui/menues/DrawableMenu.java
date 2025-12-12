7cc57777149f274e9f9bb8efb8548cf9384215e3
Noel <noelel@chalmers.se>
2025-12-12 12:02:25 +0100

    small fix

---
+++ core/src/main/java/com/IONA/TowerDefense/view/ui/menues/DrawableMenu.java
@@ -0,0 +1,9 @@
+package com.IONA.TowerDefense.view.ui.menues;
+
+import com.IONA.TowerDefense.view.Drawable;
+import com.IONA.TowerDefense.view.ui.TowerMenuDrawer;
+
+public sealed interface DrawableMenu extends Drawable permits TowerMenuDrawer {
+
+
+}
