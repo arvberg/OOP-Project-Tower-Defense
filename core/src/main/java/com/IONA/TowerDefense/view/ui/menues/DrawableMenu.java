e42e57bb9fe166a95ce62d8722b170968efd5e4a
Noel <noelel@chalmers.se>
2025-12-12 12:05:52 +0100

    Fixed so its runnable

--- core/src/main/java/com/IONA/TowerDefense/view/ui/menues/DrawableMenu.java
+++ core/src/main/java/com/IONA/TowerDefense/view/ui/menues/DrawableMenu.java
@@ -1,7 +1,7 @@
 package com.IONA.TowerDefense.view.ui.menues;
 
 import com.IONA.TowerDefense.view.Drawable;
-import com.IONA.TowerDefense.view.ui.TowerMenuDrawer;
+import com.IONA.TowerDefense.view.ui.menues.TowerMenuDrawer;
 
 public sealed interface DrawableMenu extends Drawable permits TowerMenuDrawer {
 
---
+++ core/src/main/java/com/IONA/TowerDefense/view/ui/menues/TowerMenuDrawer.java
@@ -0,0 +1,12 @@
+package com.IONA.TowerDefense.view.ui.menues;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.awt.*;
+
+public final class TowerMenuDrawer implements DrawableMenu {
+
+    @Override
+    public void draw(SpriteBatch batch, ShapeRenderer shapeRenderer, float delta){}
+}
