c202426af314086b27ed548f1c4a8edfcc557a18
Ingmarolsen <olseningmar@gmail.com>
2025-12-18 13:05:10 +0100

    merge med ingmar_logic_2

--- core/src/main/java/com/IONA/TowerDefense/model/ui/buttonui/CancelButton.java
+++ core/src/main/java/com/IONA/TowerDefense/model/ui/buttonui/CancelButton.java
@@ -5,7 +5,7 @@ import com.IONA.TowerDefense.model.input.GameAction;
 public class CancelButton extends Button {
 
     public CancelButton(float x, float y) {
-        super(x, y, 1f, 1f, GameAction.RESTART);
+        super(x, y, 1f, 1f, GameAction.CANCEL_TOWER);
     }
 
 }
